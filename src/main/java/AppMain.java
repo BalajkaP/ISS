402b0e0c28cf710e079d70e9da0a6509374c5791
BalajkaP <p.b.1@seznam.cz>
2023-10-10 15:11:38 +0200

    Komentáře v AppMain

--- src/main/java/AppMain.java
+++ src/main/java/AppMain.java
@@ -1,4 +1,6 @@
 import java.io.IOException;
+
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import java.net.URI;
@@ -8,33 +10,55 @@ import java.net.http.HttpResponse;
 
 public class AppMain {
     public static void main(String[] args) throws IOException, InterruptedException {
-        // Create an instance of HttpClient
-        HttpClient httpClient = HttpClient.newHttpClient();
 
-        // Define the URL for the API
-        String apiUrl = "http://api.open-notify.org/astros.json";
+    // ŘEŠENÍ ÚLOHY 2 ZPŮSOBY:
 
-        // Create an HTTP GET request to the API
-        HttpRequest httpRequest = HttpRequest.newBuilder()
-                .uri(URI.create(apiUrl))
-                .build();
+    // 1. ZPŮSOB: TENTO KÓD VYUŽÍVÁ PŘÍMO HTTP DOTAZ NA DANOU STRÁNKU S API, A TA NÁM VRÁTÍ POŽADOVANÉ ÚDAJE.
+    //            NA TÉTO STRÁNCE UŽ JE VYTVOŘENÉ NĚJAKÉ ROZHRANÍ, KTERÉ KOMUNIKUJE S DB.
+    //            VRÁTÍ NÁM ÚDAJE V SOUBORU TYPU JSON, A TEN MUSÍME ZDE V KÓDU PŘEVÉST (ROZEBRAT, PARSE)
+    //            NA JSON OBJEKT - JsonObject, KTERÝ PAK POMOCÍ JsonWorker TŘÍDY NAHRAJU DO DATABÁZE
+//        // Create an instance of HttpClient
+//        HttpClient httpClient = HttpClient.newHttpClient();
+//
+//        // Define the URL for the API
+//        String apiUrl = "http://api.open-notify.org/astros.json";
+//
+//        // Create an HTTP GET request to the API
+//        HttpRequest httpRequest = HttpRequest.newBuilder()
+//                .uri(URI.create(apiUrl))
+//                .build();
+//
+//        // Send the HTTP request and get the response
+//        HttpResponse<String> response = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+//
+//        // Check if the HTTP request was successful (status code 200)
+//        if (response.statusCode() == 200) {
+//            // Parse the JSON response
+//            String responseBody = response.body();
+//            JsonObject jsonObject = JsonParser.parseString(responseBody).getAsJsonObject();
+//
+//            // Create an instance of JsonWorker
+//            JsonWorker jsonWorker = new JsonWorker();
+//
+//            // Load the JSON data into the database
+//            jsonWorker.jsonPersonLoaderToDatabase(jsonObject);
+//        } else {
+//            System.out.println("HTTP request failed with status code: " + response.statusCode());
+//        }
 
-        // Send the HTTP request and get the response
-        HttpResponse<String> response = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+    //****************************************************************************************
+    // 2. ZPŮSOB: TENTO KÓD POUŽÍVÁ JIŽ ZTAŽENÝ SOUBOR iss.json, který mám uložený buď někde na disku,
+    //            nebo si ho vytvořím přímo zde v resources. Mohu si ho vytvořit tak, že klik na odkaz
+    //            http://api.open-notify.org/astros.json , a zkopíruju jeho obsah.
+    //            Pak v resources vytvořím po pravý klik na main -NEW FILE - iss.json, a do něj zkopíruju obsah
+    //            souboru astros.json. Abych tedy mohl zadat relativní cestu, tak raději ten soubor dát přímo pod main
+    //
 
-        // Check if the HTTP request was successful (status code 200)
-        if (response.statusCode() == 200) {
-            // Parse the JSON response
-            String responseBody = response.body();
-            JsonObject jsonObject = JsonParser.parseString(responseBody).getAsJsonObject();
+        JsonWorker jsonWorker = new JsonWorker();
+    // Do jsonParser zadám cestu k mojemu iss.json. Dát pravý klik na iss.json vlevo v TREE v INTELLIJ, a
+    // COPY PATH/REFERENCE - Path from content Root zvolit.
+        JsonElement jsonElementPeople = jsonWorker.jsonParser("src/main/iss.json");
+        jsonWorker.jsonPersonLoaderToDatabase(jsonElementPeople);
 
-            // Create an instance of JsonWorker
-            JsonWorker jsonWorker = new JsonWorker();
-
-            // Load the JSON data into the database
-            jsonWorker.jsonPersonLoaderToDatabase(jsonObject);
-        } else {
-            System.out.println("HTTP request failed with status code: " + response.statusCode());
-        }
     }
 }
