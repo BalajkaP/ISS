f2d4790b08e2f001e809a29a8fefe181e5c9a441
BalajkaP <p.b.1@seznam.cz>
2023-10-15 00:17:35 +0200

    Dokončení kodu pro ISS-Now

--- src/main/java/AppMainISSNow.java
+++ src/main/java/AppMainISSNow.java
@@ -17,45 +17,59 @@ public class AppMainISSNow {
     //We created a IssTimeLocation Hibernate entity to map the database table.
     //The main method sends an HTTP request to "http://api.open-notify.org/iss-now.json" and parses the JSON response.
     //The jsonTimeLocationLoaderToDatabase method extracts latitude, longitude, and timestamp data from the JSON response and inserts it into the IssTimeLocation table
-    //timestamp in the JSON response is not in a format that can be directly parsed by LocalDateTime. The timestamp is likely
+    //The timestamp in the JSON response is not in a format that can be directly parsed by LocalDateTime. The timestamp is likely
     // in epoch seconds, and you need to convert it to a LocalDateTime object correctly.
     //In this code, we use Instant.ofEpochSecond to convert the epoch seconds to an Instant, and then we use LocalDateTime.ofInstant
-    // to convert the Instant to a LocalDateTime. This should handle the timestamp correctly.
+    // to convert the Instant to a LocalDateTime.
     public static void main(String[] args) throws IOException, InterruptedException {
-        HttpClient httpClient = HttpClient.newHttpClient();
-
-        // Define the URL for the API
-        String apiUrl = "http://api.open-notify.org/iss-now.json";
-
-        // Create an HTTP GET request to the API
-        HttpRequest httpRequest = HttpRequest.newBuilder()
-                .uri(URI.create(apiUrl))
-                .build();
-
-        // Send the HTTP request and get the response
-        HttpResponse<String> response = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
 
         try {
-            Session session = DbConnect.getSession();
+            Session session = DbConnect.getSession();             // Vytvořím session
             Transaction transaction = session.beginTransaction(); // Begin a new transaction to group database operations together.
 
-            // Check if the HTTP request was successful (status code 200)
-            if (response.statusCode() == 200) {
-                // Parse the JSON response
-                JsonObject jsonObject = JsonParser.parseString(response.body()).getAsJsonObject();
+            HttpClient httpClient = HttpClient.newHttpClient();
 
-                // Create an instance of JsonWorker
-                JsonWorkerISSNow jsonWorker = new JsonWorkerISSNow();
+            // Define the URL for the API
+            String apiUrl = "http://api.open-notify.org/iss-now.json";
 
-                // Load the JSON data into the database
-                jsonWorker.jsonTimeLocationLoaderToDatabase(session, jsonObject);
+            // Create an HTTP GET request to the API
+            HttpRequest httpRequest = HttpRequest.newBuilder()
+                    .uri(URI.create(apiUrl))
+                    .build();
 
-            } else {
-                System.out.println("HTTP request failed with status code: " + response.statusCode());
+            // Jelikož chci přečíst z URL data o poloze 2x (potřebuji 2 řádky v tabulce), tak zde musím dát for cyklus.
+            for (int i = 0; i < 2; i++) {
+
+                // Send the HTTP request and get the response
+                HttpResponse<String> response = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+
+                // Check if the HTTP request was successful (status code 200)
+                if (response.statusCode() == 200) {
+                    // Parse the JSON response
+                    JsonObject jsonObject = JsonParser.parseString(response.body()).getAsJsonObject();
+
+                    // Create an instance of JsonWorker
+                    JsonWorkerISSNow jsonWorker = new JsonWorkerISSNow();
+
+                    // Load the JSON data into the database
+                    jsonWorker.jsonTimeLocationLoaderToDatabase(session, jsonObject);
+
+                } else {
+                    System.out.println("HTTP request failed with status code: " + response.statusCode());
+                }
+                if (i==0) Thread.sleep(2000);
             }
-
             transaction.commit(); // Commit the transaction, which applies the changes to the database.
             System.out.println("Data were loaded to the database.");
+
+            // Zde volám metodu issspeed(), kde provedu nejprve výpočet rozdílu času. Pak výpočet location (location=(longitude+latitude)/2) pro
+            // každý řádek tabulky IssTimeLocation (location1 a 2). Pak rozdíl location2- location1.
+            // Nakonec výpočet: speed= timestampDifference[s] / locationdifference[m]
+            transaction.begin();
+            VariousDbQuery variousDbQuery = new VariousDbQuery();
+            variousDbQuery.issspeed(session);
+
+            transaction.commit();
             session.close(); // Close the JPA session to release resources and end the database connection.
         } catch (Exception e) {
             e.printStackTrace();
